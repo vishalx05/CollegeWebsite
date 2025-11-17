d2cbb174a3ec2406a367565366222f18fa7b18c9
vishalx05 <vishalmauryaa1313@gmail.com>
2025-11-17 17:54:02 +0530

    install antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import 'remixicon/fonts/remixicon.css'
 import 'animate.css';
+import '@ant-design/v5-patch-for-react-19';
 const App = () => {
   return (
     <div>App</div>
